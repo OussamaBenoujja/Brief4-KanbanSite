2970e389cafe337fb521548dc8c8a82a66c13085
OussamaBenoujja <osama2code79@gmail.com>
2024-10-28 17:28:06 +0100

    finished the front end styling for the kanban page

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+
+
+
+// Initialization for ES Users
+import {
+    Modal,
+    Ripple,
+    initTWE,
+  } from "tw-elements";
+  
+  initTWE({ Modal, Ripple });
+
+
+const btnModal = document.getElementById("modalbtn");
+const modal = document.getElementById("crud-modal");
+
+
+  
